c8a2908668a06329b33c13304b8773dab47ea401
Guillaume HARTEMANN-PIOLLET <ghartemann@com-company.fr>
2023-12-16 15:00:25 +0100

    test

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+A
+build/NoGoZone.js,e\3\e387e79e456e0e67e02ddc411a0385711b4324fd
+<
+src/index.js,f\9\f900edc44cb929c2267a16100a32851f6d5b477d
+=
+src/module.js,1\b\1b9ddd3bc86009244045e34fe53d405daa7331e5
--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 // Expose the OpenRCT2 to Visual Studio Code's Intellisense
 /// <reference path="OPENRCT2PATH/bin/openrct2.d.ts" />
 
-// Import a module from another file.
-import Message from "./module";
+import Oui from "./OliUI"
 
-function main() {
-    console.log(Message); // Display the imported "Hallo World" message from module.js.
+function CreateWindow()
+{
+    const window = new Oui.Window("My Window");
+    window.setWidth(300);
+
+    const label = new Oui.Widgets.Label("No Go Zones");
+    window.addChild(label);
+
+    const groupbox = new Oui.GroupBox("Set up a no go zone");
+    window.addChild(groupbox);
+
+    const button = new Oui.Widgets.Button("Start", () => {
+        // Implementing callbacks is a breeze.
+        defineNoGoZone();
+        console.log('test');
+    });
+    groupbox.addChild(button);
+
+    window.open();
 }
 
+function defineNoGoZone() {
+    console.log('test');
+    // Get the tile under the mouse cursor
+    // var tile = ui.tileSelection.tile;
+    // // Get the tile element
+    // var tileElement = map.getTile(tile.x, tile.y);
+    // // Set the tile element to be a no go zone
+    // tileElement.flags |= TileFlags.NoEntry;
+    // // Update the tile element
+    // map.setTile(tile.x, tile.y, tileElement);
+}
+
+var main = function() {
+    // Add a menu item under the map icon on the top toolbar
+    ui.registerMenuItem("No go zone", function() {
+        CreateWindow();
+    });
+};
+
 registerPlugin({
-    name: "MYPLUGINNAME",
-    version: "0.1",
+    name: "NoGoZone",
+    version: "0.1.0",
     licence: "MIT", // Make sure to set the license prior to release
-    authors: [""],
+    authors: ["NotKrankor"],
     type: "local",
     main: main
 });
